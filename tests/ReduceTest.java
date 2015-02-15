98647bc94356ed9f6673af7f40684374c8ef7bc2
EricChristensen <christensen.t.eric@gmail.com>
2015-02-15 14:29:53 -0600

    refractored algorithm into a single function

--- tests/ReduceTest.java
+++ tests/ReduceTest.java
@@ -2,8 +2,33 @@ import java.util.Random;
 
 public class ReduceTest {
 
-    public void reduce() {
-
+    public static void reduce(int[] days) {
+	int greatestSpikeA = 0;
+        int greatestSpikeB = 0;
+        int greatestSpikeMagnitude = 0;
+        for (int i = 0; i < days.length; i++) {
+            int lookBacks = 5;
+            if (i <= lookBacks) {
+                lookBacks = i;
+            }
+            int currentGreatestSpike = 0;
+            int currentA = 0;
+            int currentB = 0;
+            for (int j = 0; j < lookBacks; j++) {
+                if (days[i] - days[i - j] > currentGreatestSpike) {
+                    currentGreatestSpike = days[i] - days[i - j];
+                    currentA = i - j;
+                    currentB = i;
+                }
+            }
+            if (currentGreatestSpike > greatestSpikeMagnitude) {
+                greatestSpikeMagnitude = currentGreatestSpike;
+                greatestSpikeA = currentA;
+                greatestSpikeB = currentB;
+            }
+        }
+        System.out.println("The Greatest spike is a value of: " + greatestSpikeMagnitude);
+        System.out.println("This spike is from day " + greatestSpikeA + " to day " + greatestSpikeB);
     }
     
     public static void main(String args[]) {
@@ -15,31 +40,9 @@ public class ReduceTest {
 	    days[i] = random.nextInt(1000);
 	    System.out.println("Position: " + i + " " + days[i]);
 	}
-	int greatestSpikeA = 0;
-	int greatestSpikeB = 0;
-	int greatestSpikeMagnitude = 0;
-	for (int i = 0; i < days.length; i++) {
-	    int lookBacks = 5;
-	    if (i <= lookBacks) {
-		lookBacks = i;
-	    }
-	    int currentGreatestSpike = 0;
-	    int currentA = 0;
-	    int currentB = 0;
-	    for (int j = 0; j < lookBacks; j++) {
-		if (days[i] - days[i - j] > currentGreatestSpike) {
-		    currentGreatestSpike = days[i] - days[i - j];
-		    currentA = i - j;
-		    currentB = i;
-	        }
-	    }
-	    if (currentGreatestSpike > greatestSpikeMagnitude) {
-		greatestSpikeMagnitude = currentGreatestSpike;
-		greatestSpikeA = currentA;
-		greatestSpikeB = currentB;
-	    }
-	}
-	System.out.println("The Greatest spike is a value of: " + greatestSpikeMagnitude);
-	System.out.println("This spike is from day " + greatestSpikeA + " to day " + greatestSpikeB);
+	reduce(days);
+	days[10] = 0;
+	days[13] = 100000;
+	reduce(days);
     }
 }
