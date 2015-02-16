8e2583615efd968385c701dbafc9cb1285739b52
David Van Loon <david.van.loon@outlook.com>
2015-02-16 14:36:20 -0600

    Changed mapper to be its own class

---
+++ hadoop-wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob1Mapper.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package cs5621.hadoop.wikistats;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ * Mapper for Job 1
+ * @author David Van Loon
+ *
+ */
+public class WikiStatsJob1Mapper extends
+		Mapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT> {
+
+}
