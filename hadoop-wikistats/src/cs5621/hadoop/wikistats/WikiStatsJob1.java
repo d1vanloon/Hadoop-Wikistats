8ef836f04387323a140d38a202a3cecc0a7d45b0
David Van Loon <david.van.loon@outlook.com>
2015-02-16 11:25:32 -0600

    Added WikiStatsJob1 class

---
+++ hadoop-wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob1.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package cs5621.hadoop.wikistats;
+
+/**
+ * WikiStatsJob1
+ * The first job in the WikiStats analysis.
+ * @author David Van Loon
+ *
+ */
+public class WikiStatsJob1 {
+
+}
