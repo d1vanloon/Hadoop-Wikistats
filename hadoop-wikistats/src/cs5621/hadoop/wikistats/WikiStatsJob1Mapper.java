703955cd085d03c026d5b02c68cbef4e50b02233
David Van Loon <david.van.loon@outlook.com>
2015-02-16 14:55:27 -0600

    Added some parsing code and constants

--- hadoop-wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob1Mapper.java
+++ hadoop-wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob1Mapper.java
@@ -16,6 +16,10 @@ import org.apache.hadoop.mapreduce.Mapper;
  */
 public class WikiStatsJob1Mapper extends
 		Mapper<LongWritable, Text, Text, Text> {
+	
+	private static final int LANGUAGE_INDEX = 0;
+	private static final int PAGE_NAME_INDEX = 1;
+	private static final int VIEWS_INDEX = 2;
 
 	/* (non-Javadoc)
 	 * @see org.apache.hadoop.mapreduce.Mapper#map(java.lang.Object, java.lang.Object, org.apache.hadoop.mapreduce.Mapper.Context)
@@ -24,7 +28,25 @@ public class WikiStatsJob1Mapper extends
 	protected void map(LongWritable key, Text value,
 			Mapper<LongWritable, Text, Text, Text>.Context context)
 			throws IOException, InterruptedException {
-		super.map(key, value, context);
+		
+		/*
+		 * Data exists in the following format:
+		 * 	domain page_title count_views total_response_size
+		 * For example:
+		 * 	en Main_Page 42 50043
+		 * Other domains can exist, for example:
+		 * 	de.d Freiheit 176 314159
+		 * This comes from wiktionary.org and will not be considered.
+		 * Only two letter language codes are to be considered.
+		 */
+		
+		// Grab the current line as a String from the input value
+		String line = value.toString();
+		// Split the line into tokens by spaces
+		String[] tokens = line.split("[ ]+");
+		// We should have 4 tokens
+		assert(tokens.length == 4);
+		
 	}
 
 	
