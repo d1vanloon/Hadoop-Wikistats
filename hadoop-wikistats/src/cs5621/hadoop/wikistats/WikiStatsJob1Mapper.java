7f03ff6c2c064da398bc605ab814233d0379bd3b
David Van Loon <david.van.loon@outlook.com>
2015-02-16 14:43:39 -0600

    Added map function

--- hadoop-wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob1Mapper.java
+++ hadoop-wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob1Mapper.java
@@ -3,14 +3,29 @@
  */
 package cs5621.hadoop.wikistats;
 
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 /**
- * Mapper for Job 1
+ * Mapper for Job 1.
  * @author David Van Loon
  *
  */
 public class WikiStatsJob1Mapper extends
-		Mapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT> {
+		Mapper<LongWritable, Text, Text, Text> {
 
+	/* (non-Javadoc)
+	 * @see org.apache.hadoop.mapreduce.Mapper#map(java.lang.Object, java.lang.Object, org.apache.hadoop.mapreduce.Mapper.Context)
+	 */
+	@Override
+	protected void map(LongWritable key, Text value,
+			Mapper<LongWritable, Text, Text, Text>.Context context)
+			throws IOException, InterruptedException {
+		super.map(key, value, context);
+	}
+
+	
 }
