7cf2f74374258fc53bcc47b41442f8bde65f9f6e
David Van Loon <david.van.loon@outlook.com>
2015-02-16 18:58:07 -0600

    Added test code to WikiStatsJob1Mapper

--- hadoop-wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob1Mapper.java
+++ hadoop-wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob1Mapper.java
@@ -5,10 +5,16 @@ package cs5621.hadoop.wikistats;
 
 import java.io.IOException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 
 /**
  * Mapper for Job 1.
@@ -108,5 +114,21 @@ public class WikiStatsJob1Mapper extends
 		}
 	}
 
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 2) {
+		      System.err.println("Usage: WikiStatsJob1Mapper <in> <out>");
+		      System.exit(2);
+		    }
+		    Job job = new Job(conf, "word count");
+		    job.setJarByClass(WikiStatsJob1Mapper.class);
+		    job.setMapperClass(WikiStatsJob1Mapper.class);
+		    job.setOutputKeyClass(Text.class);
+		    job.setOutputValueClass(Text.class);
+		    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		    System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 	
 }
