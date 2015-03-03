87247b47afbc7b05c6d385a1a54ed8aa3a15efec
Yan <baixx195@d.umn.edu>
2015-03-02 18:41:20 -0600

    1.Add WikiStatsJob3.java

---
+++ Hadoop-Wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob3.java
@@ -0,0 +1,36 @@
+package cs5621.hadoop.wikistats;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WikiStatsJob3 {
+  
+  //Mapper class for finding top M language
+	public static class Job3Mapper extends Mapper<Object, Text, Text, IntWritable>{
+		
+			public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      		String line = value.toString();
+    			String lang = line.substring(0,2);  		
+   				context.write( new Text(lang), new IntWritable(1) );
+			}
+				
+	}
+
+	//Reducer class for finding top M langauge
+  public static class Job3Reducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+    
+    	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {  
+					int counter = 0;
+
+					for(IntWritable value : values){
+						counter = counter + value.get();
+					}	
+			
+					context.write(key, new IntWritable(counter));
+  		}
+
+	}
+
+}
