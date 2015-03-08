831809a9ac1e6c3ad1b57669c2d1205c831fd878
Yan <baixx195@d.umn.edu>
2015-03-07 18:51:46 -0600

    1.Update WikiStatJob3.java. The previous version is wrote on Mac OS. It does not work on Itasca/Panfs since there will be some strange characters such as (^M).

--- Hadoop-Wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob3.java
+++ Hadoop-Wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob3.java
@@ -1,38 +1,35 @@
 package cs5621.hadoop.wikistats;
 
 import java.io.IOException;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class WikiStatsJob3 {
-  
-  //Mapper class for finding top M language
+public class WikiStatsJob3{
+
 	public static class Job3Mapper extends Mapper<Object, Text, Text, IntWritable>{
-		
-			public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-      		String line = value.toString();
-    			String lang = line.substring(0,2);  		
-   				context.write( new Text(lang), new IntWritable(1) );
-			}
-				
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+						          String line = value.toString();
+											          String lang = line.substring(0,2);
+																          context.write( new Text(lang), new IntWritable(1) );
+		}
+	
 	}
+	
+	public static class Job3Reducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 
-	//Reducer class for finding top M langauge
-  public static class Job3Reducer extends Reducer<Text,IntWritable,Text,IntWritable> {
-    
-    	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {  
-					int counter = 0;
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+						          int counter = 0;
 
-					for(IntWritable value : values){
-						counter = counter + value.get();
-					}	
-			
-					context.write(key, new IntWritable(counter));
-  		}
+											          for(IntWritable value : values){
+																				            counter = counter + value.get();
+																										          }
 
+																															          context.write(key, new IntWritable(counter));
+																																				      }
+	
 	}
 
 }
