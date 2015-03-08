192e7b927166521579367fcc490bb16b4681933c
Yan <baixx195@d.umn.edu>
2015-03-07 22:13:28 -0600

    1.Delete redundant indents in WikiStatsJob4.java

--- Hadoop-Wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob4.java
+++ Hadoop-Wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob4.java
@@ -32,7 +32,7 @@ public class WikiStatsJob4{
 	public static class Job4Mapper extends Mapper<Object, Text, IntWritable, Text>{
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-								context.write( new IntWritable(0), value );
+			context.write( new IntWritable(0), value );
 		}
 	
 	}
@@ -41,87 +41,87 @@ public class WikiStatsJob4{
 
 		public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 											
-											Configuration conf = context.getConfiguration();
-											int M = Integer.parseInt(conf.get(WikiStats.LANGUAGES_PARAM_NAME));
-											
-											//This array is used for storing top M languages in descending order. Just code of languages.
-											String[] topLangs = new String[M];					
-											
-											//Used for sorting all languages to find top M languages
-											TreeMap<Integer, String> allLangs = new TreeMap<Integer, String>();					
-											
-											//HashMap to store top N pages for all languages. We will go through
-											//this map and pick just records for top M languages.
-										  ArrayList<String> allPages = new ArrayList<String>();
-											
-											//Go through all input datas.
-											//Push language data(output of job 3) to allLangs TreeMap.
-											//Push page data(output of job 2) to allPages HashMap. 
-											String str = null;
-											String[] strs = null;
-											for(Text value : values){
-															str = value.toString();
-															strs = str.split(";");
-															if(strs.length == 2) {
-																	allLangs.put(Integer.parseInt(strs[1]), strs[0]);
-															} else if(strs.length == 3) {
-																	allPages.add(value.toString());	
-															}
-											}
+			Configuration conf = context.getConfiguration();
+			int M = Integer.parseInt(conf.get(WikiStats.LANGUAGES_PARAM_NAME));
+			
+			//This array is used for storing top M languages in descending order. Just code of languages.
+			String[] topLangs = new String[M];					
+			
+			//Used for sorting all languages to find top M languages
+			TreeMap<Integer, String> allLangs = new TreeMap<Integer, String>();					
+			
+			//HashMap to store top N pages for all languages. We will go through
+			//this map and pick just records for top M languages.
+			ArrayList<String> allPages = new ArrayList<String>();
+			
+			//Go through all input datas.
+			//Push language data(output of job 3) to allLangs TreeMap.
+			//Push page data(output of job 2) to allPages HashMap. 
+			String str = null;
+			String[] strs = null;
+			for(Text value : values){
+							str = value.toString();
+							strs = str.split(";");
+							if(strs.length == 2) {
+									allLangs.put(Integer.parseInt(strs[1]), strs[0]);
+							} else if(strs.length == 3) {
+									allPages.add(value.toString());	
+							}
+			}
 
-											/**
-											 * In the TreeMap, key is the number of unique page, value is the code of language.
-											 * And TreeMap sort data in ascending order, so we just need to pop datas on the head,
-											 * and keey last M items. That will be top M languages.
-											 */
-											int numOfLangs = allLangs.size();
-											for(int i=0; i<numOfLangs-M; i++){
-													allLangs.pollFirstEntry();
-											}
+			/**
+			 * In the TreeMap, key is the number of unique page, value is the code of language.
+			 * And TreeMap sort data in ascending order, so we just need to pop datas on the head,
+			 * and keey last M items. That will be top M languages.
+			 */
+			int numOfLangs = allLangs.size();
+			for(int i=0; i<numOfLangs-M; i++){
+					allLangs.pollFirstEntry();
+			}
 
-											//Push top M language codes to array for furthur use.
-											int CopyOfM = M;
-											for(Entry langEntry : allLangs.entrySet()){
-													String langCode = (String)langEntry.getValue();
-													topLangs[CopyOfM-1] = langCode;
-													CopyOfM--;	
-											}
-											
-											/**
-											 * Create HashMap with size of M. Each item in HashMap is a linkedlist
-											 * to store top N pages. 
-											 */
-										  HashMap<String, LinkedList> pagesMap = new HashMap<String, LinkedList>();
-											for(int j=0; j<topLangs.length; j++){
-													pagesMap.put(topLangs[j], new LinkedList<String>());
-											}
-											
-											// Push top N pages of each languages to HashMap.
-											for(int k=0; k<allPages.size(); k++){
-													String line = allPages.get(k);
-													String[] strsInLine = line.split(";");
-													String codeOfLang = strsInLine[0];
-													String nameOfPage = strsInLine[1];
-													String accessOfPage = strsInLine[2];
-													if(pagesMap.containsKey(codeOfLang))pagesMap.get(codeOfLang).add(nameOfPage+";"+accessOfPage);
-											}
+			//Push top M language codes to array for furthur use.
+			int CopyOfM = M;
+			for(Entry langEntry : allLangs.entrySet()){
+					String langCode = (String)langEntry.getValue();
+					topLangs[CopyOfM-1] = langCode;
+					CopyOfM--;	
+			}
+			
+			/**
+			 * Create HashMap with size of M. Each item in HashMap is a linkedlist
+			 * to store top N pages. 
+			 */
+			HashMap<String, LinkedList> pagesMap = new HashMap<String, LinkedList>();
+			for(int j=0; j<topLangs.length; j++){
+					pagesMap.put(topLangs[j], new LinkedList<String>());
+			}
+			
+			// Push top N pages of each languages to HashMap.
+			for(int k=0; k<allPages.size(); k++){
+					String line = allPages.get(k);
+					String[] strsInLine = line.split(";");
+					String codeOfLang = strsInLine[0];
+					String nameOfPage = strsInLine[1];
+					String accessOfPage = strsInLine[2];
+					if(pagesMap.containsKey(codeOfLang))pagesMap.get(codeOfLang).add(nameOfPage+";"+accessOfPage);
+			}
 
 
-											// write top N pages of top M languages to output
-											for(int m=0; m<topLangs.length; m++){
-													LinkedList pageList = pagesMap.get(topLangs[m]);
-													while(!pageList.isEmpty()){
-															context.write( new Text(topLangs[m]), new Text((String)pageList.poll()) );
-													}
-											}
+			// write top N pages of top M languages to output
+			for(int m=0; m<topLangs.length; m++){
+					LinkedList pageList = pagesMap.get(topLangs[m]);
+					while(!pageList.isEmpty()){
+							context.write( new Text(topLangs[m]), new Text((String)pageList.poll()) );
+					}
+			}
 
-											//write top M languages to output
-											while(!allLangs.isEmpty()){
-													Entry entryTopLang = allLangs.pollLastEntry();
-													String accessOfTopLang = Integer.toString((Integer)entryTopLang.getKey());
-													String codeOfTopLang = (String)entryTopLang.getValue();
-													context.write(new Text(codeOfTopLang), new Text(accessOfTopLang));
-											}
+			//write top M languages to output
+			while(!allLangs.isEmpty()){
+					Entry entryTopLang = allLangs.pollLastEntry();
+					String accessOfTopLang = Integer.toString((Integer)entryTopLang.getKey());
+					String codeOfTopLang = (String)entryTopLang.getValue();
+					context.write(new Text(codeOfTopLang), new Text(accessOfTopLang));
+			}
 											
 		
 		}
