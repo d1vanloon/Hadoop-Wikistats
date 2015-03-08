f3ee9c32406662d02e0fc38beee213de49f7ce22
Yan <baixx195@d.umn.edu>
2015-03-07 19:16:22 -0600

    1.Update WikiStats.java. 1.1 Use waitForCompletion to replece JobConf. The reason is they can complete same job but JobConf will disable task progerss display in generic.e. 1.2 Use semicolon as seperator. 1.3 Add Job 4 to replace post-processing function. 1.4 For job1 and job2, set num of reduce tasks to 64 to accelerate the reduce progress.

--- Hadoop-Wikistats/src/cs5621/hadoop/wikistats/WikiStats.java
+++ Hadoop-Wikistats/src/cs5621/hadoop/wikistats/WikiStats.java
@@ -2,6 +2,8 @@ package cs5621.hadoop.wikistats;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -9,6 +11,27 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
 import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import java.net.URI;
 
 /**
  * Job Control class and main
@@ -28,10 +51,6 @@ public class WikiStats {
 	 * Parameter name for accessing the parameter NumberOfPages.
 	 */
 	static final String PAGES_PARAM_NAME = "NumberOfPages";
-	/**
-	 * Name of temporary output directory for Job 1.
-	 */
-	private static final String JOB1_OUTPUT_TEMP = "/job1";
 	/**
 	 * Argument index of the input path.
 	 */
@@ -53,6 +72,10 @@ public class WikiStats {
 	 */
 	private static final int PERIOD_ARGS_INDEX = 4;
 	/**
+	 * Job 1 output directory.
+	 */
+	private static final String JOB_1_SUBFOLDER = "/job1";
+	/**
 	 * Job 2 output directory.
 	 */
 	private static final String JOB_2_SUBFOLDER = "/job2";
@@ -60,6 +83,10 @@ public class WikiStats {
 	 * Job 3 output directory.
 	 */
 	private static final String JOB_3_SUBFOLDER = "/job3";
+	/**
+	 * Job 4 output directory.
+	 */
+	private static final String JOB_4_SUBFOLDER = "/job4";
 
 	public static void main(String[] args) throws Exception {
 
@@ -75,27 +102,36 @@ public class WikiStats {
 		// Path set up
 
 		Path job1InputPath = new Path(args[INPUT_ARGS_INDEX]);
-		Path job1OutputPath = new Path(args[OUTPUT_ARGS_INDEX] + JOB1_OUTPUT_TEMP);
+		Path job1OutputPath = new Path(args[OUTPUT_ARGS_INDEX] + JOB_1_SUBFOLDER);
 		Path job2InputPath = job1OutputPath;
 		Path job2OutputPath = new Path(args[OUTPUT_ARGS_INDEX] + JOB_2_SUBFOLDER);
 		Path job3InputPath = job1OutputPath;
 		Path job3OutputPath = new Path(args[OUTPUT_ARGS_INDEX] + JOB_3_SUBFOLDER);
+		Path job4OutputPath = new Path(args[OUTPUT_ARGS_INDEX] + JOB_4_SUBFOLDER);
 
 		// Configuration set up
 
 		Configuration conf1 = new Configuration();
 		Configuration conf2 = new Configuration();
 		Configuration conf3 = new Configuration();
+		Configuration conf4 = new Configuration();
 
+		conf1.set("mapred.textoutputformat.separator", ";");
 		conf1.set(PAGES_PARAM_NAME, pages);
 		conf1.set(LANGUAGES_PARAM_NAME, languages);
 		conf1.set(PERIOD_PARAM_NAME, period);
+		conf2.set("mapred.textoutputformat.separator", ";");
 		conf2.set(PAGES_PARAM_NAME, pages);
 		conf2.set(LANGUAGES_PARAM_NAME, languages);
 		conf2.set(PERIOD_PARAM_NAME, period);
+		conf3.set("mapred.textoutputformat.separator", ";");
 		conf3.set(PAGES_PARAM_NAME, pages);
 		conf3.set(LANGUAGES_PARAM_NAME, languages);
 		conf3.set(PERIOD_PARAM_NAME, period);
+		conf4.set("mapred.textoutputformat.separator", ";");
+		conf4.set(PAGES_PARAM_NAME, pages);
+		conf4.set(LANGUAGES_PARAM_NAME, languages);
+		conf4.set(PERIOD_PARAM_NAME, period);
 
 		// Job 1 set up
 
@@ -109,30 +145,35 @@ public class WikiStats {
 		job1.setReducerClass(WikiStatsJob1.Job1Reducer.class);
 		job1.setOutputKeyClass(Text.class);
 		job1.setOutputValueClass(Text.class);
+		job1.setNumReduceTasks(64);
 		FileInputFormat.addInputPath(job1, job1InputPath);
 		FileOutputFormat.setOutputPath(job1, job1OutputPath);
-
+		job1.waitForCompletion(true);
+		
 		// Job 2 set up
 		
 		/*
 		 * Job 2 finds the top N pages for each language.
 		 */
-
 		Job job2 = new Job(conf2, "job2");
 		job2.setJarByClass(WikiStats.class);
 		job2.setMapperClass(WikiStatsJob2.Job2Mapper.class);
 		job2.setReducerClass(WikiStatsJob2.Job2Reducer.class);
+		job2.setPartitionerClass(WikiStatsJob2.SortSpikePartitioner.class);
+		job2.setGroupingComparatorClass(WikiStatsJob2.GroupingComparator.class);
+		job2.setSortComparatorClass(WikiStatsJob2.SortComparator.class);
 		job2.setOutputKeyClass(Text.class);
 		job2.setOutputValueClass(Text.class);
+		job2.setNumReduceTasks(64);
 		FileInputFormat.addInputPath(job2, job2InputPath);
 		FileOutputFormat.setOutputPath(job2, job2OutputPath);
+		job2.waitForCompletion(true);
 		
 		// Job 3 set up
 		
 		/*
 		 * Job 3 finds the top M languages.
 		 */
-		
 		Job job3 = new Job(conf3, "job3");
 		job3.setJarByClass(WikiStats.class);
 		job3.setMapperClass(WikiStatsJob3.Job3Mapper.class);
@@ -141,14 +182,34 @@ public class WikiStats {
 		job3.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job3, job3InputPath);
 		FileOutputFormat.setOutputPath(job3, job3OutputPath);
-
+		job3.waitForCompletion(true);
+			
+		// Job 4 set up
+		
+		/*
+		 * Job 4 process outputs of Job 2 and Job 3 to generate final output.
+		 */
+		Job job4 = new Job(conf4, "job4");
+		job4.setJarByClass(WikiStats.class);
+		job4.setMapperClass(WikiStatsJob4.Job4Mapper.class);
+		job4.setReducerClass(WikiStatsJob4.Job4Reducer.class);
+		job4.setMapOutputKeyClass(IntWritable.class);
+		job4.setMapOutputValueClass(Text.class);
+		job4.setOutputKeyClass(Text.class);
+		job4.setOutputValueClass(Text.class);
+		FileInputFormat.addInputPath(job4, job3OutputPath);
+		FileInputFormat.addInputPath(job4, job2OutputPath);
+		FileOutputFormat.setOutputPath(job4, job4OutputPath);
+		job4.waitForCompletion(true);
+		
+			
 		// Controlled job set up, dependency for jobs 2, 3
-
+		/*
 		ControlledJob cJob1 = new ControlledJob(job1.getConfiguration());
-		ControlledJob cJob2 = new ControlledJob(job3.getConfiguration());
+		ControlledJob cJob2 = new ControlledJob(job2.getConfiguration());
 		ControlledJob cJob3 = new ControlledJob(job3.getConfiguration());
 		cJob1.setJob(job1);
-		cJob2.setJob(job3);
+		cJob2.setJob(job2);
 		cJob3.setJob(job3);
 		cJob2.addDependingJob(cJob1);
 		cJob3.addDependingJob(cJob1);
@@ -169,13 +230,9 @@ public class WikiStats {
 		while(!jobControl.allFinished())
 			Thread.sleep(5000);
 		
-		
-		/*
-		 * POST-PROCESSING GOES HERE
-		 */
-		
-		// TODO: Process output of jobs 2, 3
+		*/
 
 		System.exit(0);
 	}
+
 }
