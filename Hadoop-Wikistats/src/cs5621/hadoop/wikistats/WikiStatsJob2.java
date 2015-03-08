ffdf5acbb064c584b3bc0e188c2e6c163de0316a
Yan <baixx195@d.umn.edu>
2015-03-07 18:56:32 -0600

    1.Add spike to key to implement SecondarySort. 2.Simplify the reduce function. 3.Catch exception in SortComparator in line 108. This exception must be handled, otherwise we will not get output of Job2.

--- Hadoop-Wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob2.java
+++ Hadoop-Wikistats/src/cs5621/hadoop/wikistats/WikiStatsJob2.java
@@ -3,14 +3,15 @@ package cs5621.hadoop.wikistats;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.NumberFormatException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -28,28 +29,25 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class WikiStatsJob2 {
 
-	public static class Job2Mapper extends Mapper<Text, Text, Text, Text>{
-    
-    	private Text word = new Text();
-
+	public static class Job2Mapper extends Mapper<LongWritable, Text, Text, Text>{
 
     	//key format = "EnPageName"
     	//value format = "1238" spike in text
     	@Override
-    	public void map(Text key, Text value, Context context)
+    	public void map(LongWritable key, Text value, Context context)
 						throws IOException, InterruptedException {
 	
-		String line = key.toString();
-		String spike = value.toString();
+		String line = value.toString();
+		String[] strs = line.split(";");
 
-		//String length = strs.length + " ";
-		String lang = line.substring(0, 2);
-		String page = line.substring(2);
+		String lang = strs[0].substring(0, 2);
+		String page = strs[0].substring(2);
+		String spike = strs[1];
 
 		//Convert IntWritable value to String
 	
-		Text outputKey = new Text(lang);
-		Text outputValue = new Text(page + "," + spike);
+		Text outputKey = new Text(lang + spike);
+		Text outputValue = new Text(page + ";" + spike);
 
 		context.write(outputKey, outputValue);
     	}
@@ -59,8 +57,8 @@ public class WikiStatsJob2 {
 	//We include spike in key,but we need to partition data only by lang code in key.
 	public static class SortSpikePartitioner extends Partitioner<Text, Text>{
 
-	@Override
-       	public int getPartition(Text key, Text value, int numPartitions){
+		@Override
+		public int getPartition(Text key, Text value, int numPartitions){
 			return (key.toString().substring(0,2).hashCode()) % numPartitions;
 		}
 
@@ -102,9 +100,16 @@ public class WikiStatsJob2 {
 			
 			String aLang = aString.substring(0,2);
 			String bLang = bString.substring(0,2);
-			int aSpike = Integer.parseInt(aString.substring(2));
-			int bSpike = Integer.parseInt(bString.substring(2));
-	
+
+			int aSpike = 1;
+			int bSpike = 1;
+			
+			try{
+					aSpike = Integer.parseInt(aString.substring(2));
+					bSpike = Integer.parseInt(bString.substring(2));
+			}catch(NumberFormatException e){
+					e.printStackTrace();	
+			}
 			if(!aLang.equals(bLang))
 				return aLang.compareTo(bLang); 
 			else
@@ -121,28 +126,23 @@ public class WikiStatsJob2 {
 
 			Configuration conf = context.getConfiguration();
 			int numberOfPages = Integer.parseInt(conf.get(WikiStats.PAGES_PARAM_NAME));
-			String language = key.toString();
+			//numberOfPages = 5;
+			String lang = key.toString().substring(0,2);
 
 			int i = 0;
 
 			if(numberOfPages > 0){
 
 				for(Text val : values){
-					String temp = val.toString();
-					int split = temp.lastIndexOf(",");
-					String page = temp.substring(0, split);
-					String spike = temp.substring(split + 1);
-
-					Text outputKey = new Text(language + page);
-					Text outputValue = new Text(spike);
-
-					context.write(outputKey, outputValue);
+					context.write(new Text(lang), val);
 					i++;
 
 					if(i >= numberOfPages)
 						break;
 				}
 			}
+
 		}
 	}
+
 }
